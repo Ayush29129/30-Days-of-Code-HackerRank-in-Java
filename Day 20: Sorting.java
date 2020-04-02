60009a4adc9fd0149c1fb0610ba76b77377193c5
Ayush Gupta <48252204+Ayush29129@users.noreply.github.com>
2020-04-02 23:09:33 +0530

    Create Day 20: Sorting.java

---
+++ Day 20: Sorting.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+        int temp,count=0;
+        for(int j=0;j<n;j++){
+            for(int i=0;i<n-1;i++){
+                if(a[i]>a[i+1]){
+                    temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                    count++;
+                }
+            }
+        }
+        System.out.println("Array is sorted in "+count+" swaps.");
+        System.out.println("First Element: "+a[0]);
+        System.out.println("Last Element: "+a[a.length-1]);
+    }
+}
